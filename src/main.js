24e62959bd8b21538c45c3ff0a3219adcdc6dc84
taodtu <taodtu.gmail.com>
2019-09-20 11:05:09 +0100

    config vuetify

--- src/main.js
+++ src/main.js
@@ -2,21 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from "./store";
 import router from "./router";
-import Vuetify from "vuetify";
 import vuetify from './plugins/vuetify';
 
-Vue.use(Vuetify, {
-  theme: {
-    // BLUE AND YELLOW
-    primary: "#25395b",
-    secondary: "#304b78",
-    accent: "#e6b400",
-    error: "#F44336",
-    warning: "#ffeb3b",
-    info: "#edf1f8",
-    success: "#4caf50"
-  }
-});
 
 Vue.config.performance = true;
 
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,35 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+import Vue from "vue";
+import Vuetify, {
+  VApp,
+  VNavigationDrawer,
+  VFooter,
+  VList,
+  VBtn,
+  VIcon,
+  VGrid,
+  VToolbar,
+  VTextField,
+  transitions,
+  VTabs
+} from "vuetify/lib";
 
-Vue.use(Vuetify);
-
-export default new Vuetify({
-  icons: {
-    iconfont: 'mdi',
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VNavigationDrawer,
+    VFooter,
+    VList,
+    VBtn,
+    VIcon,
+    VGrid,
+    VToolbar,
+    VTextField,
+    transitions,
+    VTabs
   },
+  iconFont: "fa"
 });
+
+const opts = {};
+
+export default new Vuetify(opts);
