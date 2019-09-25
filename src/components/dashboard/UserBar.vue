8a10033c631429fd22506b5608d9b402199e69de
taodtu <taodtu.gmail.com>
2019-09-25 15:51:38 +0100

    style the userbar

--- src/components/dashboard/UserBar.vue
+++ src/components/dashboard/UserBar.vue
@@ -1,15 +1,16 @@
 <template>
-    <div>
-    Current User: {{user}}
-    </div>
+  <div>
+    Current User:
+    <div class="mt-3">{{user}}</div>
+  </div>
 </template>
 
 <script>
-import {mapGetters} from "vuex"
-    export default {
-        name:"UserBar",
-        computed: {
+import { mapGetters } from "vuex";
+export default {
+  name: "UserBar",
+  computed: {
     ...mapGetters(["user"])
-  },
-    }
+  }
+};
 </script>
